906a1ef8476adf1b0e191f968f25bee65ae97fe6
Valentin Misiaszek <mowerv6@gmail.com>
2021-03-18 00:46:19 +0100

    feat(result): add pie chart display mode onn result

--- src/components/ChoiceResult.tsx
+++ src/components/ChoiceResult.tsx
@@ -1,9 +1,24 @@
-import { Box, Button, Flex, Heading, keyframes } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  keyframes,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { CSSObject } from "@emotion/react";
+import { Group } from "@visx/group";
+import { scaleOrdinal } from "@visx/scale";
+import { Pie } from "@visx/shape";
+import { PieArcDatum } from "@visx/shape/lib/shapes/Pie";
 import React, { useEffect, useMemo, useState } from "react";
 import { getChoiceById } from "../api/choices.api";
 import { IChoice } from "../types";
 import ResultLine from "./ResultLine";
+import { BsFillPieChartFill } from "react-icons/bs";
+import { RiBarChartHorizontalFill } from "react-icons/ri";
 
 interface ChoiceResultProps {
   choiceObjectId: string;
@@ -19,6 +34,8 @@ const ChoiceResult: React.FC<ChoiceResultProps> = ({
   isLastChoice,
 }) => {
   const [choice, setChoice] = useState<IChoice>();
+  const [chartCanBeDisplayed] = useMediaQuery("(min-width: 720px)");
+  const [isResultChart, setIsResultChart] = useState<boolean>(true);
 
   const calculChosenRateForProposals = (choice: IChoice) => ({
     ...choice,
@@ -61,9 +78,17 @@ const ChoiceResult: React.FC<ChoiceResultProps> = ({
         padding: "2rem",
         width: "70vw",
         maxWidth: "500px",
+        minHeight: "562px",
       };
     return { ...base, borderRadius: "50%", height: "80px", width: "80px" };
   }, [isOpen]);
+
+  const pieData = choice?.proposals.map((propo) => {
+    return {
+      name: propo.title,
+      value: propo.rate,
+    };
+  });
 
   return (
     <>
@@ -101,19 +126,47 @@ const ChoiceResult: React.FC<ChoiceResultProps> = ({
             direction="column"
             w="100%"
             align="space-around"
-            justify="center"
+            justify="space-around"
+            height="450px"
           >
-            {choice?.proposals.map((propo, i) => (
-              <Box my={[2, 2, 5]} key={"result" + i}>
-                <ResultLine label={propo.title} rate={propo.rate!} />
-              </Box>
-            ))}
+            {choice && chartCanBeDisplayed && isResultChart ? (
+              <MyPie data={pieData} width={450} height={450} />
+            ) : (
+              choice?.proposals.map((propo, i) => (
+                <Box my={[2, 2, 5]} key={"result" + i}>
+                  <ResultLine label={propo.title} rate={propo.rate!} />
+                </Box>
+              ))
+            )}
           </Flex>
         )}
         {isOpen && isLastChoice() === false ? (
-          <Button onClick={onNextClick} colorScheme="blue" mt={2}>
-            Next choice
-          </Button>
+          <Flex mt={2}>
+            {chartCanBeDisplayed && (
+              <>
+                {isResultChart ? (
+                  <IconButton
+                    colorScheme="whatsapp"
+                    mx={2}
+                    aria-label="Toggle pie"
+                    icon={<Icon as={RiBarChartHorizontalFill} />}
+                    onClick={() => setIsResultChart(false)}
+                  />
+                ) : (
+                  <IconButton
+                    colorScheme="whatsapp"
+                    mx={2}
+                    aria-label="Toggle line"
+                    icon={<Icon as={BsFillPieChartFill} />}
+                    onClick={() => setIsResultChart(true)}
+                  />
+                )}
+              </>
+            )}
+            <Button onClick={onNextClick} colorScheme="blue">
+              Next choice
+            </Button>
+          </Flex>
         ) : (
           isLastChoice() &&
           isOpen && (
@@ -128,3 +181,91 @@ const ChoiceResult: React.FC<ChoiceResultProps> = ({
 };
 
 export default ChoiceResult;
+
+const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+type ChoicePieData = {
+  name: string;
+  value: number;
+};
+
+type PieProps = {
+  data: any;
+  width: number;
+  height: number;
+  margin?: typeof defaultMargin;
+};
+
+function MyPie({ data, width, height, margin = defaultMargin }: PieProps) {
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const radius = Math.min(innerWidth, innerHeight) / 2;
+  const centerY = innerHeight / 2;
+  const centerX = innerWidth / 2;
+  const top = centerY + margin.top;
+  const left = centerX + margin.left;
+
+  const getProposalsFrequencyColor = scaleOrdinal({
+    domain: data.map((propo: ChoicePieData) => propo.name),
+    range: [
+      "rgba(43,108,176,1)",
+      "rgba(43,108,176,0.8)",
+      "rgba(43,108,176,0.6)",
+      "rgba(43,108,176,0.4)",
+    ],
+  });
+
+  const frequency = (d: ChoicePieData) => d.value;
+
+  return (
+    <svg width={width} height={height}>
+      <Group top={top} left={left}>
+        <Pie data={data} pieValue={frequency} outerRadius={radius}>
+          {(pie) => {
+            return pie.arcs.map((arc: PieArcDatum<ChoicePieData>, index) => {
+              const { name } = arc.data;
+              const [centroidX, centroidY] = pie.path.centroid(arc);
+              const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+              const arcPath = pie.path(arc);
+              const arcFill = getProposalsFrequencyColor(name);
+              return (
+                <g key={`arc-${name}-${index}`}>
+                  <path
+                    d={arcPath !== null ? arcPath : undefined}
+                    fill={arcFill}
+                  />
+                  {hasSpaceForLabel && (
+                    <>
+                      <text
+                        x={centroidX}
+                        y={centroidY}
+                        dy=".33em"
+                        fill="#ffffff"
+                        fontSize={18}
+                        textAnchor="middle"
+                        pointerEvents="none"
+                      >
+                        {arc.data.name}
+                      </text>
+                      <text
+                        x={centroidX}
+                        y={centroidY + 30}
+                        dy=".33em"
+                        fill="#ffffff"
+                        fontSize={18}
+                        textAnchor="middle"
+                        pointerEvents="none"
+                      >
+                        {Math.round(arc.data.value).toString()}%
+                      </text>
+                    </>
+                  )}
+                </g>
+              );
+            });
+          }}
+        </Pie>
+      </Group>
+    </svg>
+  );
+}
